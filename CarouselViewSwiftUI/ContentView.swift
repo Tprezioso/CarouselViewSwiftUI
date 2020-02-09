a8bf0ec6b53b2a6582739174ac84776fd43edff6
Tprezioso <tommyprezioso@gmail.com>
2020-02-09 13:20:41 -0500

    added images and fixed some bugs

--- CarouselViewSwiftUI/ContentView.swift
+++ CarouselViewSwiftUI/ContentView.swift
@@ -10,9 +10,61 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        ScrollView {
+            // Comment this section below to get the screen shot for first step
+            GeometryReader { geometry in
+                ImageCarouselView(numberOfImages: 3) {
+                    Image("husky-1")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: geometry.size.width, height: geometry.size.height)
+                        .clipped()
+                    Image("husky-2")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: geometry.size.width, height: geometry.size.height)
+                        .clipped()
+                    Image("husky-3")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: geometry.size.width, height: geometry.size.height)
+                        .clipped()
+                }
+            }.frame(height: 300, alignment: .center)
+            // Uncomment this section above for 3rd step
+            
+            VStack(alignment: .leading, spacing: 10) {
+                HStack {
+                    Image("swiftTom")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 55, height: 55)
+                        .clipShape(Circle())
+                        .shadow(radius: 4)
+                    
+                    VStack(alignment: .leading) {
+                        Text("Written By")
+                            .foregroundColor(.gray)
+                        Text("Thomas Prezioso Jr")
+                    }
+                }
+                
+                Text("Feb 10 2020")
+                    .foregroundColor(.gray)
+                
+                Text("How to Make a Image Carousel in SwiftUI")
+                
+                Text(loremIpsum)
+                    .lineLimit(nil)
+            }
+            .padding(.horizontal)
+            .padding(.top, 16.0)
+        }.edgesIgnoringSafeArea(.all)
     }
 }
+let loremIpsum = """
+Lorem ipsum dolor sit amet consectetur adipiscing elit donec, gravida commodo hac non mattis augue duis vitae inceptos, laoreet taciti at vehicula cum arcu dictum. Cras netus vivamus sociis pulvinar est erat, quisque imperdiet velit a justo maecenas, pretium gravida ut himenaeos nam. Tellus quis libero sociis class nec hendrerit, id proin facilisis praesent bibendum vehicula tristique, fringilla augue vitae primis turpis.
+"""
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
